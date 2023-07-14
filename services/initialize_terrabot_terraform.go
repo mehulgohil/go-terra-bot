322ba4e7e4bebbf637241ce7d72e4b9623a89c17
Mehul Gohil <mehulgohil75@gmail.com>
2023-07-14 15:36:15 +0530

    optimize the conditions

--- services/initialize_terrabot_terraform.go
+++ services/initialize_terrabot_terraform.go
@@ -3,12 +3,11 @@ package services
 import (
 	"log"
 	"os"
-	"strings"
 )
 
 func InitializeTerraformFolders() {
 	err := os.Mkdir("terrabot-tf", os.ModePerm)
-	if err != nil && !strings.Contains(err.Error(), "Cannot create a file when that file already exists") {
+	if err != nil && !os.IsExist(err) {
 		log.Fatal(err)
 	}
 }
